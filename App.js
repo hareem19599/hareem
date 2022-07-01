8e2876f26cfc7e3bebe7abe1012b220d5b72bee6
Hareem Fatima <59992893+hareem19599@users.noreply.github.com>
2022-06-30 13:40:54 -1200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import DataFrontend from './DataFrontend';
+function App() {
+  return (
+    <div className="App">
+      <DataFrontend />
+    </div>
+  );
+}
+
+export default App;
